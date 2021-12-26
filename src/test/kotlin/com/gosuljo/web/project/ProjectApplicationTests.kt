978b9478aea399ac0c89267c46be14873a10e016
gosuljo <lurutia@naver.com>
2021-12-27 00:28:08 +0900

    first commit backend

---
+++ src/test/kotlin/com/gosuljo/web/project/ProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gosuljo.web.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
